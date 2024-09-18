db2223a655f850148578bb248ac38eccca303bd8
gawhite92 <122578880+gawhite92@users.noreply.github.com>
2024-09-18 21:45:24 +0800

    Working project. Just needs local storage.

--- src/index.js
+++ src/index.js
@@ -145,28 +145,145 @@ function activateListeners() {
         })
     }
 
-        //PROJECT EDIT BUTTONS
+    //PROJECT EDIT BUTTONS
     const projectEditButtons = document.querySelectorAll(".projectEditButton");
 
     for (let i = 0; i < projectEditButtons.length; i++) {
         projectEditButtons[i].addEventListener("click", () => {
+            const toDoListHeader = document.getElementById("toDoListHeader");
             const parent = projectEditButtons[i].parentElement;
-            console.log(parent)
-            const child = document.getElementsByClassName("editFormTitleInput")
-            console.log(child)
-
+            const child = document.getElementById("editFormTitleInput")
 
             if (parent.contains(child)) {
-                console.log('hi');
+                console.log(toDoListHeader.textContent == database[i].title);
+
+                if (toDoListHeader.textContent == database[i].title) { //if the open project is being renamed
+                    toDoListHeader.textContent = child.value;
+                }
+
+                database[i].title = child.value;
+                child.remove();
+                clearProjects();
+                populateProjects();
+                activateListeners();
+                console.table(database);
                 return
             }
 
             const selectedEditButton = projectEditButtons[i];
             const editFormTitleInput = document.createElement('input');
-            editFormTitleInput.setAttribute("class", "editFormTitleInput")
+            editFormTitleInput.setAttribute("id", "editFormTitleInput");
+            editFormTitleInput.setAttribute("value", `${database[i].title}`);
             selectedEditButton.parentElement.append(editFormTitleInput);
         })
     }
+
+    // TASK EDIT BUTTONS
+
+    const taskEditButtons = document.querySelectorAll(".edittodobutton");
+    const taskEditFormContainer = document.querySelectorAll(".todocontainer");
+
+    for (let i = 0; i < taskEditButtons.length; i++) {
+        taskEditButtons[i].addEventListener("click", () => {
+            const selectedTaskEditForm = document.getElementById('taskEditForm');
+            const selectedEditFormContainer = taskEditFormContainer[i];
+            console.log(selectedTaskEditForm);
+            console.log(selectedEditFormContainer);
+            
+            if (selectedTaskEditForm){
+                selectedTaskEditForm.remove();
+                return
+            }
+
+            const taskEditForm = document.createElement("form");
+            taskEditForm.setAttribute("id", "taskEditForm");
+
+            const titleInput = document.createElement("input");
+            titleInput.setAttribute("type", "text");
+            titleInput.setAttribute("id", "taskTitleInput");
+            titleInput.setAttribute("placeholder", "Title");
+            titleInput.setAttribute("required", "");
+            titleInput.setAttribute("value", `${database[getSelectedProject().selectedProject].tasks[i].title}`);
+
+            const descriptionInput = document.createElement("input");
+            descriptionInput.setAttribute("type", "text");
+            descriptionInput.setAttribute("id", "taskDescriptionInput");
+            descriptionInput.setAttribute("placeholder", "Description");
+            descriptionInput.setAttribute("required", "");
+            descriptionInput.setAttribute("value", `${database[getSelectedProject().selectedProject].tasks[i].description}`);
+
+            const prioritySelector = document.createElement("select");
+            prioritySelector.setAttribute("name", "priority");
+            prioritySelector.setAttribute("id", "taskPriorityInput");
+
+            const priorityOption1 = document.createElement("option");
+            priorityOption1.setAttribute("value", "Normal");
+            priorityOption1.textContent = "Normal";
+
+            const priorityOption2 = document.createElement("option");
+            priorityOption2.setAttribute("value", "High");
+            priorityOption2.textContent = "High";
+
+            const priorityOption3 = document.createElement("option");
+            priorityOption3.setAttribute("value", "Urgent");
+            priorityOption3.textContent = "Urgent";
+
+            const dueDateInput = document.createElement("input");
+            dueDateInput.setAttribute("type", "date");
+            dueDateInput.setAttribute("id", "taskDueDateInput");
+            dueDateInput.setAttribute("placeholder", "Due Date");
+            dueDateInput.setAttribute("required", "");
+            dueDateInput.setAttribute("value", `${database[getSelectedProject().selectedProject].tasks[i].dueDate}`);
+
+
+            const submitEditTaskButton = document.createElement("button");
+            submitEditTaskButton.textContent = "Submit";
+            submitEditTaskButton.setAttribute("id", "submitEditTask");
+            submitEditTaskButton.setAttribute("type", "button");
+
+            taskEditFormContainer[i].appendChild(taskEditForm);
+
+            taskEditForm.appendChild(titleInput);
+            taskEditForm.appendChild(descriptionInput);
+            taskEditForm.appendChild(prioritySelector);
+            prioritySelector.appendChild(priorityOption1);
+            prioritySelector.appendChild(priorityOption2);
+            prioritySelector.appendChild(priorityOption3);
+            taskEditForm.appendChild(dueDateInput);
+            taskEditForm.appendChild(submitEditTaskButton);
+
+            submitEditTaskButton.addEventListener("click", () => {
+                console.log('Submitted edited task!')
+                const selectedTask = database[getSelectedProject().selectedProject].tasks[i];
+                console.log(selectedTask);
+
+                if (dueDateInput.value == '') {
+                    alert('Invalid date');
+                    return
+                }
+            
+                const formattedDueDate = format(dueDateInput.value, "dd/MM/yyyy")
+            
+                if (titleInput.value == '') {
+                    alert('Title required')
+                    return
+                }
+
+                selectedTask.title = titleInput.value;
+                selectedTask.description = descriptionInput.value;
+                selectedTask.priority = prioritySelector.value;
+                selectedTask.dueDate = formattedDueDate;
+
+
+                clearTasks();
+                populateTasks();
+                activateListeners();
+            })
+        })
+    }
+
+
+
 
     //CHECKBOXES
 
@@ -177,17 +294,17 @@ function activateListeners() {
             const selectedProject = database[getSelectedProject().selectedProject]
             const selectedTask = checkboxes[i];
 
-            if(selectedProject.tasks[i].active){
-            selectedProject.tasks[i].active = false;
-            //Visual changes occur temporarily without reloading entire task list, but are reloaded the same way if project list changes.
-            selectedTask.setAttribute("class", "todocheckbox complete");
-            selectedTask.parentElement.parentElement.setAttribute("class", "todocontainer complete")
+            if (selectedProject.tasks[i].active) {
+                selectedProject.tasks[i].active = false;
+                //Visual changes occur temporarily without reloading entire task list, but are reloaded the same way if project list changes.
+                selectedTask.setAttribute("class", "todocheckbox complete");
+                selectedTask.parentElement.parentElement.setAttribute("class", "todocontainer complete")
             } else {
                 selectedProject.tasks[i].active = true;
                 selectedTask.setAttribute("class", "todocheckbox");
                 selectedTask.parentElement.parentElement.setAttribute("class", "todocontainer")
-            }            
-            
+            }
+
             // clearTasks();
             // populateTasks(getSelectedProject().selectedProject);
             // activateListeners();
